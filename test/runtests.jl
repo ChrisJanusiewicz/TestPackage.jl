a9b201adab614efb6030ff97ceb38f6d7d55ea28
Chris Janusiewicz <kpjanusiewicz@gmail.com>
2020-07-21 10:45:34 +0100

    Add tests for Riemann functions

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,33 @@
 using TestPackage
 using Test
 
+
 @testset "TestPackage.jl" begin
-    # Write your tests here.
+    # integration parameters
+    a = 0
+    b = 1
+    n = 1000
+
+    # Error tolerance for testing
+    # total_error < interval_width / (n_samples * 0.1)
+    total_error = (b - a) / (n * 0.1)
+
+    f(x) = x^2
+    F(x) = x^3/3
+    res = F(b) - F(a) # actual result
+
+    lr = left_riemann(f, a, b, n)
+    rr = right_riemann(f, a, b, n)
+    tr = trapezoid(f, a, b, n)
+
+
+    @test abs(lr - res) < total_error
+    @test abs(rr - res) < total_error
+    @test abs(tr - res) < total_error
+
+    # @printf("left \t%.4f  %.4f\n", lr, res - lr)
+    # @printf("right\t%.4f  %.4f\n", rr, res - rr)
+    # @printf("trap \t%.4f  %.4f\n", tr, res - tr)
+
+
 end
